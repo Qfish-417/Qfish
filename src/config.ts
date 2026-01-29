778bc09487394bc2541f9420a502d119bbad4fa6
扶曦 <2320283299@qq.com>
2026-01-29 17:03:53 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -19,12 +19,12 @@ import { LinkPreset } from "./types/config";
 // 移除i18n导入以避免循环依赖
 
 // 定义站点语言
-const SITE_LANG = "ja"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
+const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "Mizuki",
 	subtitle: "One demo website",
-	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://Qfish.golang.com/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
@@ -118,7 +118,7 @@ export const siteConfig: SiteConfig = {
 				"/assets/desktop-banner/2.webp",
 				"/assets/desktop-banner/3.webp",
 				"/assets/desktop-banner/4.webp",
-				"/assets/desktop-banner/5.webp",
+				"/assets/desktop-banner/5.webp"，
 				"/assets/desktop-banner/6.webp",
 			], // 桌面横幅图片
 			mobile: [
@@ -163,7 +163,7 @@ export const siteConfig: SiteConfig = {
 				"君ってさ、知らないうちに私の毎日になってたよ",
 				"君と話すと、なんか毎日がちょっと楽しくなるんだ",
 				"今日はなんでもない日。でも、ちょっとだけいい日",
-			],
+			]，
 			typewriter: {
 				enable: true, // 启用副标题打字机效果
 
